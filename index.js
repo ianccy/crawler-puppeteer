85d369475a1692ea0ca118a125bceae73bdbbf44
ian <ian@mobagel.com>
2023-12-06 11:55:38 +0800

    Add crawl array function

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const puppeteer = require("puppeteer-extra");
 const stealthPlugin = require("puppeteer-extra-plugin-stealth");
 
-async function getPrice() {
+async function getPrice(year) {
   puppeteer.use(stealthPlugin());
 
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   try {
     const url =
-      "https://www.wine-searcher.com/find/louis+jadot+clos+st+jacques+gevrey+chambertin+premier+cru+cote+de+nuit+burgundy+france/2018/taiwan";
+      "https://www.wine-searcher.com/find/louis+jadot+clos+st+jacques+gevrey+chambertin+premier+cru+cote+de+nuit+burgundy+france/" +
+      year +
+      "/taiwan";
 
     await page.goto(url);
 
@@ -27,4 +29,54 @@ async function getPrice() {
   }
 }
 
-getPrice();
+const arr = [
+  "2022",
+  "2021",
+  "2020",
+  "2019",
+  "2018",
+  "2017",
+  "2016",
+  "2015",
+  "2014",
+  "2013",
+  "2012",
+  "2011",
+  "2010",
+  "2009",
+  "2008",
+  "2007",
+  "2006",
+  "2005",
+  "2004",
+  "2003",
+  "2002",
+  "2001",
+  "2000",
+  "1999",
+  "1998",
+  "1997",
+  "1996",
+  "1995",
+  "1994",
+  "1993",
+  "1992",
+  "1991",
+  "1990",
+  "1989",
+  "1988",
+  "1985",
+  "1966",
+];
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function processYears() {
+  for (let index = 0; index < arr.length; index++) {
+    const year = arr[index];
+    await getPrice(year);
+    await delay(3000); // Delay for 1 second (1000 milliseconds)
+  }
+}
+
+processYears();
